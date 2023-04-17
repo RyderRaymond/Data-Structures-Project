121facc6321c083239c197b9771eaf9563b877de
Ryder Fedora <ryderraymond@live.com>
2023-04-17 17:55:19 -0400

    sort by date

--- Data Structures Project/src/Database.java
+++ Data Structures Project/src/Database.java
@@ -71,7 +71,7 @@ public class Database {
         jeansLL = new LinkedList<>();
         for (int i = 0; i < numJeans; i++)
         {
-            jeansLL.add(new ProductJeans("Jeans" + i, random.nextDouble(), new Date()));
+            jeansLL.add(new ProductJeans("Jeans" + i, random.nextDouble(15, 50), new Date()));
         }
 
         //Initial capacity is set to jeans * 2 to avoid collisions. Uses extra space but will be faster
@@ -100,7 +100,7 @@ public class Database {
 
         for (int i = 0; i < num; i++)
         {
-            ProductShirt next = new ProductShirt("Shirt" + i, random.nextDouble(), new Date());
+            ProductShirt next = new ProductShirt("Shirt" + i, random.nextDouble(15, 50), new Date());
             shirtMap.put(next.getTitle(), next);
             shirtPrices.add(next);
             shirtDates.add(next);
@@ -117,7 +117,7 @@ public class Database {
 
         for (int i = 0; i < num; i++)
         {
-            ProductTShirt next = new ProductTShirt("TShirt" + i, random.nextDouble(), new Date());
+            ProductTShirt next = new ProductTShirt("TShirt" + i, random.nextDouble(15, 50), new Date());
             tShirtMap.put(next.getTitle(), next);
             tShirtDates.add(next);
             tShirtPrices.add(next);
@@ -133,7 +133,7 @@ public class Database {
 
         for (int i = 0; i < num; i++)
         {
-            ProductShorts next = new ProductShorts("Shorts" + i, random.nextDouble(), new Date());
+            ProductShorts next = new ProductShorts("Shorts" + i, random.nextDouble(15, 50), new Date());
             shortsMap.put(next.getTitle(), next);
             shortsPrices.add(next);
             shortsDates.add(next);
@@ -149,7 +149,7 @@ public class Database {
 
         for (int i = 0; i < num; i++)
         {
-            ProductHat next = new ProductHat("Hat" + i, random.nextDouble(), new Date());
+            ProductHat next = new ProductHat("Hat" + i, random.nextDouble(15, 50), new Date());
             hatMap.put(next.getTitle(), next);
             hatPrices.add(next);
             hatDates.add(next);
@@ -165,7 +165,7 @@ public class Database {
 
         for (int i = 0; i < num; i++)
         {
-            ProductShoes next = new ProductShoes("Shoes" + i, random.nextDouble(), new Date());
+            ProductShoes next = new ProductShoes("Shoes" + i, random.nextDouble(15, 50), new Date());
             shoesMap.put(next.getTitle(), next);
             shoesPrices.add(next);
             shoesDates.add(next);
@@ -181,7 +181,7 @@ public class Database {
 
         for (int i = 0; i < num; i++)
         {
-            ProductJacket next = new ProductJacket("Jacket" + i, random.nextDouble(), new Date());
+            ProductJacket next = new ProductJacket("Jacket" + i, random.nextDouble(15, 50), new Date());
             jacketMap.put(next.getTitle(), next);
             jacketDates.add(next);
             jacketPrices.add(next);
@@ -207,7 +207,7 @@ public class Database {
         return jeansLL.get(position);
     }
 
-    //using Linked Hashmap
+    //using Linked Hashmap - the main implementation
     public ProductJeans searchJeans(String name)
     {
         if (jeansMap.containsKey(name))
@@ -219,7 +219,7 @@ public class Database {
         return null;
     }
 
-    //Linked Map does not have a method for
+    //Searching by index using linked hashMap
     public ProductJeans getJeans(int position)
     {
         ProductJeans[] j = jeansMap.values().toArray(new ProductJeans[0]);
@@ -230,6 +230,7 @@ public class Database {
         return null;
     }
 
+    //Sorts this product by prices using quicksort
     public void sortPrices(String productType)
     {
         //Initializes as jeansPrices so java does not get mad that list may not be initialized
@@ -263,6 +264,41 @@ public class Database {
         Sorting.quickSortByPrice(list);
     }
 
+    //sorts this product by date using quicksort
+    public void sortDates(String productType)
+    {
+        //Initializes as jeansPrices so java does not get mad that list may not be initialized
+        ArrayList<Product> list = jeansDates;
+
+        switch(productType)
+        {
+            case "jeans":
+                list = jeansDates;
+                break;
+            case "shirt":
+                list = shirtDates;
+                break;
+            case "tshirt":
+                list = tShirtDates;
+                break;
+            case "shorts":
+                list = shortsDates;
+                break;
+            case "hat":
+                list = hatDates;
+                break;
+            case "shoes":
+                list = shoesDates;
+                break;
+            case "jacket":
+                list = jacketDates;
+                break;
+        }
+
+        Sorting.quickSortByDate(list);
+    }
+
+    //Prints all products in this list
     public void printAll(ArrayList<Product> list)
     {
         for (Product p : list){
