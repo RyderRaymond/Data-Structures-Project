00648ed4d6b7349279e2d7fc536ab1c98a5c20f8
jcajuste <108548408+jcajuste@users.noreply.github.com>
2023-04-20 09:31:55 -0400

    simple change

--- Data Structures Project/src/DecisionHandler.java
+++ Data Structures Project/src/DecisionHandler.java
@@ -4,7 +4,7 @@ public class DecisonHandler{
     //
     public static getCategory (String task){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please choose a Item for" + task + ":");
+        System.out.println("Please choose a proper Item for" + task + ":");
         String category = scanner.nextLine();
         category = category.toLowerCase();
         return category;
