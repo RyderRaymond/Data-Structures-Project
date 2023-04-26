4b6b2bfb120969d1a3dc424d41dcaf3ff95a0e1c
Midoyle <midoyle@hartford.edu>
2023-04-26 02:32:27 -0400

    delete and unique item attributes
    I added my parts including the code to have dates from past 2 years and selected prices, also added the method for deleting an item

--- Data Structures Project/src/ProductJeans.java
+++ Data Structures Project/src/ProductJeans.java
@@ -8,29 +8,41 @@ public class ProductJeans extends Product {
 
     public ProductJeans(String title, double price, Date date)
     {
-      int selectprice;
-      swich(selectprice.rand(5))   {
-        case 0: price = 31.99
-          case 1: 
-        price = 29.99;
-        break;
-          case 2: 
-        price = 32.50;
-        break;
-          case 3: 
-        price = 30;
-        break;
-          case 4: 
-        price = 35.99;
-        break;
-          case 5: 
-        price = 28.50;
-        break;
-      }
-      
+      //  int selectprice = 0;
+      //       Random rand = new Random();
+      //       selectprice=rand.nextInt(5);
+      //       switch(selectprice)   {
+      //       case 0:
+      //           price = 31.99;
+      //           break;
+      //       case 1:
+      //           price = 29.99;
+      //           break;
+      //       case 2:
+      //           price = 32.50;
+      //           break;
+      //       case 3:
+      //           price = 30;
+      //           break;
+      //       case 4:
+      //           price = 35.99;
+      //           break;
+      //       case 5:
+      //           price = 28.50;
+      //           break;
+      //   }
         
         super(title, price, date);
     }
 
+    public static void main(String[] args) {
+      jeansLL = new LinkedList<>();
+        for (int i = 0; i < numJeans; i++) {
+            jeansLL.add(new ProductJeans("Jeans" + i, random.nextDouble(15, 50), new Date()));
+        }
+        System.out.print(jeansLL);
+
+    }
+  
 
 }
